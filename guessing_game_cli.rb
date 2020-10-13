3e81395b8b897a6a1de61048540dcee40bcd18eb
Kat Leight <leightkt@gmail.com>
2020-10-13 21:41:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,9 +2,9 @@
 #   rand(1..6)
 # end
 
-def prompt_user
-  puts "Guess a number 1 to 6!"
-end
+# def prompt_user
+#   puts "Guess a number 1 to 6!"
+# end
 
 def capture_input
   input = gets.chomp
